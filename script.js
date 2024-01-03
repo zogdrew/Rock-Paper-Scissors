0d0992742bd33345987452cf6b274f6dd26f0807
zoglex <101883103+zoglex@users.noreply.github.com>
2024-01-03 17:03:33 +0200

    save current progress

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
+const choices = ["rock","paper","scissors"]
+
 function game () {
-    //play the game
-    //play five rounds
-    //console based
+    playRound();
 }
 
 function playRound() {
-    const playerSelection =
+const playerSelection = playerChoice();
+const computerSelection = computerChoice;
 }
 
+function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while(input == null){
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if(check == true){
+        console.log(input)
+    }
+}
+
+
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+
+function validateInput(choice){
+    if (choices.includes(choice)){
+        return true;
+    } else {
+        return false
+    }
+}
+
+game();
