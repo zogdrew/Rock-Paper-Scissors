432829765cb17d0678796c1457451bf812c09028
zoglex <101883103+zoglex@users.noreply.github.com>
2024-01-03 17:20:56 +0200

    new

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-const choices = ["rock","paper","scissors"]
+const choices = ["rock", "paper", "scissors"];
 
-function game () {
-    playRound();
+function game() {
+  playRound();
 }
 
 function playRound() {
-const playerSelection = playerChoice();
-const computerSelection = computerChoice;
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice;
 }
 
 function playerChoice() {
-    let input = prompt("Type Rock, Paper, or Scissors");
-    while(input == null){
-        input = prompt("Type Rock, Paper, or Scissors");
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input)
-    if(check == true){
-        console.log(input)
-    }
+  let input = prompt("Type Rock, Paper, or Scissors");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Type Rock, Paper,Scissors. Spelling needs to be exact, but capitalization doesn't matter"
+    );
+    input = input.toLowerCase()
+    check = validateInput(input);
+  }
 }
-
-
 
 function computerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)]
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function validateInput(choice){
-    if (choices.includes(choice)){
-        return true;
-    } else {
-        return false
-    }
+function validateInput(choice) {
+  return choices.includes(choice);
 }
 
 game();
